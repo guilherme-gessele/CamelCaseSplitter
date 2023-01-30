0c0c3be011ad668e8726b8e7a5be2b1ee9004350
guilherme-gessele <guilherme.gessele@eldorado.org.br>
2023-01-30 14:16:32 -0300

    refactor: splitter algorithm

--- src/main/java/CamelCaseSplitter.java
+++ src/main/java/CamelCaseSplitter.java
@@ -1,11 +1,52 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CamelCaseSplitter {
 
+
     public static List<String> converterCamelCase(String original) {
-        return Arrays.stream(original.split("((?=[A-Z]))"))
-                .map(String::toLowerCase)
+
+        var strings = new ArrayList<String>();
+        var isPreviousLowerCase = false;
+        var builder = new StringBuilder();
+
+        for (var i = 0; i < original.length(); i++) {
+
+            var currentChar = original.charAt(i);
+
+            if (i == original.length() - 1) {
+                builder.append(currentChar);
+                strings.add(builder.toString());
+                continue;
+            }
+
+            if (isPreviousLowerCase && Character.isUpperCase(currentChar)) {
+                isPreviousLowerCase = false;
+                strings.add(builder.toString());
+
+                builder = new StringBuilder().append(currentChar);
+                continue;
+            }
+
+            if (Character.isLowerCase(currentChar)) {
+                isPreviousLowerCase = true;
+            }
+
+            builder.append(currentChar);
+        }
+
+        return strings.stream()
+                .map(s -> {
+                    var containsLowerCase = s.chars().anyMatch(Character::isLowerCase);
+
+                    if (Character.isUpperCase(s.charAt(0)) && containsLowerCase) {
+                        return s.toLowerCase();
+                    }
+
+                    return s;
+                })
                 .toList();
+
     }
 }
