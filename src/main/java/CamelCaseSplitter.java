5686f333058ca3184e26ead78acfa697f457b456
guilherme-gessele <guilherme.gessele@eldorado.org.br>
2023-01-30 09:33:49 -0300

    test: starting with uppercase

--- src/main/java/CamelCaseSplitter.java
+++ src/main/java/CamelCaseSplitter.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 public class CamelCaseSplitter {
     public static List<String> converterCamelCase(String original) {
-        return List.of(original);
+        return List.of(original.toLowerCase());
     }
 }
--- src/test/java/CamelCaseSplitterTest.java
+++ src/test/java/CamelCaseSplitterTest.java
@@ -11,4 +11,10 @@ public class CamelCaseSplitterTest {
         var response = CamelCaseSplitter.converterCamelCase("nome");
         assertEquals(List.of("nome"), response);
     }
+
+    @Test
+    void starting_with_uppercase() {
+        var response = CamelCaseSplitter.converterCamelCase("Nome");
+        assertEquals(List.of("nome"), response);
+    }
 }
